742a96402361d754d579ac1646041febf726349f
Ma0oo <belkapbigiy@gmail.com>
2021-09-14 13:55:51 +0300

    Добавлены стрелки в управлении

--- Assets/Scripts/Services/IInputs/ButtonInputUI.cs
+++ Assets/Scripts/Services/IInputs/ButtonInputUI.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace Services.IInputs
 {
-    public class ButtonInputUI : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+    public class ButtonInputUI : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         public event Action ClickUp;
         public event Action ClickDown;
         
-        public void OnPointerUp(PointerEventData eventData) => ClickUp?.Invoke();
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            ClickDown?.Invoke();
+            Debug.Log("Down");
+        }
 
-        public void OnPointerDown(PointerEventData eventData) => ClickDown?.Invoke();
+        public void OnPointerUp(PointerEventData eventData) => ClickUp?.Invoke();
     }
 }
